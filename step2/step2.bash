47907abd79abd24f45a37e01afe38caa09bee0b7
HumzaHemani <humzah@umich.edu>
2021-08-06 11:34:37 -0400

    Update step2.bash

--- step2/step2.bash
+++ step2/step2.bash
@@ -39,11 +39,11 @@ rm -f ${out}/reads.csv
 # READ IN TAGS - PIPE INTO GNU PARALLEL :
 #                       SPLIT BAM INTO SINGLE CELLS BAM Files
 echo precat
-cat ${barcode_list} | parallel --jobs $num_cores bamtools filter -in ${bigbam} -out ${out}/${sample}/${sample}.bam -tag ${tname}:{}
+cat ${barcode_list} | parallel --jobs $num_cores bamtools filter -in ${bigbam} -out ${out}/${sample}/${sample}_{}.bam -tag ${tname}:{}
 echo cat1 done
-cat ${barcode_list} | parallel --jobs $num_cores samtools sort ${out}/${sample}/${sample}.bam
+cat ${barcode_list} | parallel --jobs $num_cores samtools sort ${out}/${sample}/${sample}_{}.bam
 echo cat2 done
-cat ${barcode_list} | parallel --jobs $num_cores samtools index ${out}/${sample}/${sample}.bam
+cat ${barcode_list} | parallel --jobs $num_cores samtools index ${out}/${sample}/${sample}_{}.bam
 echo cat3 done
 LEN=$(wc -w < ${barcode_list})
 
