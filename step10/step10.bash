983f445bed7f28a58f9cb1352ccc2f257bd5c16c
Humza Hemani <humza.hemani@nih.gov>
2021-08-10 12:07:21 -0400

    step11 change

--- step10/step10.bash
+++ step10/step10.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-donor_list=${sample:def_donor_list}
+donor_csv=${sample:def_donor_csv}
 exome_sc=${exome_sc:def_exome_sc}
 output_dir=${output_dir:def_output_dir}
 
@@ -13,11 +13,11 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-LEN=$(wc -w < ${donor_list})
+LEN=$(wc -w < ${donor_csv})
 
 for i in $( seq 1 ${LEN} ); do
 echo $i
-TAG=`awk "NR==$i" ${donor_list}`
+TAG=`awk -F, "NR==$i {print \\\$1}" ${donor_csv}`
 READ_EXOME=$(samtools view -c -F 4 -L /targets_chr.bed ${exome_sc}/${TAG}_SM_bwa.bam)
 READ_SAM=$(samtools view -c -F 4 ${exome_sc}/${TAG}_SM_bwa.bam)
 COV_EXOME=$(samtools depth -b /targets_chr.bed ${exome_sc}/${TAG}_SM_bwa.bam | wc -l)
