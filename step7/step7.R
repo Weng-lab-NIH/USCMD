bc6c3700f87f92a24871d01b92f9247f32aa1a20
Humza Hemani <humza.hemani@nih.gov>
2021-08-26 12:06:53 -0400

    step 7 can deal with multiallelic stuff

--- step7/step7.R
+++ step7/step7.R
@@ -3,8 +3,10 @@
 version <- '4.10.0'
 
 # Load Libraries:
-library(tidyverse)
-library(argparse)
+suppressMessages({
+  library(tidyverse)
+  library(argparse)
+})
 
 parser <- ArgumentParser()
 parser$add_argument("sample", help="For which sample is this script being run?")
@@ -27,62 +29,61 @@ sample_cells <- sample_cells[is.na(sample_cells)==FALSE]
 donors <- rep(args$sample, length(sample_cells))
 names(donors) <- sample_cells
 
-master.annotate <- data.frame(V1="", V2="", V3=0, V4="", V5="", V6="", V7="", 
-                              V8="", V9="", V10="", V11="", donor="", barcode="",
-                              stringsAsFactors=FALSE)
+master.annotate <- data.frame(V1=character(), V2=character(), V3=numeric(), 
+  V4=character(), V5=character(), V6=character(), V7=character(), 
+  V8=character(), V9=character(), V10=character(), V11=character(), 
+  donor=character(), barcode=character(), stringsAsFactors=FALSE)
 
-#print("right before for loop")
-#print(length(sample_cells))
 for (i in 1:length(sample_cells)) {
   b <- sample_cells[i]
   filename <- paste(annotate_out, '/',sam,'_',b,'_var.ann.vcf', sep = '')
 
-  one_cell <- tryCatch({read.table(filename,sep="\t")},
+  one_cell <- tryCatch({
+      read.table(filename, sep="\t", 
+        colClasses = c('character', 'numeric', 'character', 'character',
+          'character', 'character', 'character', 'character', 'character',
+          'character', 'character'))
+    },
     error=function(cond) {
       print(paste("couldn't read ", filename, "skipping to next."))
       return("error")
     })
-  print(class(one_cell))
   if(class(one_cell)=="character"){
     next
   }
-  one_cell$V1 <- as.character(one_cell$V1)
-  one_cell$V2 <- as.numeric(one_cell$V2)
-  one_cell$V3 <- as.character(one_cell$V3)
-  one_cell$V4 <- as.character(one_cell$V4)
-  one_cell$V5 <- as.character(one_cell$V5)
-  one_cell$V6 <- as.character(one_cell$V6)
-  one_cell$V7 <- as.character(one_cell$V7)
-  one_cell$V8 <- as.character(one_cell$V8)
-  one_cell$V9 <- as.character(one_cell$V9)
-  one_cell$V10 <- as.character(one_cell$V10)
-  one_cell$V11 <- as.character(one_cell$V11)
-
+  
   # Assign Donor and Barcode:
   one_cell$donor <- sam; one_cell$barcode <- b
   master.annotate <- rbind(master.annotate, one_cell)
 }
 
-# mutations_reformat <- Reformat_Annotated_Aggregated_VCF(listcl)
-#print("master.annotate")
-#print(master.annotate)
-listcl <- master.annotate %>% 
-  mutate(ref_len = nchar(V4), alt_len = nchar(V5)) %>%
-  filter(ref_len == 1 & alt_len == 1) %>% 
+listcl <- master.annotate %>%
   separate(8,c('CONTQ','DP','ECNT','GERMQ','MBQ','MFRL','MMQ','MPOS','NALOD','NLOD','POPAF','SAAF','SAP','TLOD','ANN'),sep=';') %>%
   separate(22, as.character(seq(1,15,1)), sep='\\|')
-
 colnames(listcl) <- c('Chr','POS','ID','REF','ALT','QUAL','FILTER',
                            'CONTQ','DP','ECNT','GERMQ','MBQ','MFRL','MMQ','MPOS','NALOD','NLOD','POPAF','SAAF','SAP','TLOD','ANN',
                            'VAR_TYPE','MOD','GENE','ENSEMBL_GENE_ID','READ_TYPE','ENST_ID','FUNCTION','ratio','NUC_CHANGE','AA_CHANGE',
-                           'ratio1','ratio2','ratio3','number','stat1','stat2','stat3','donor','bc','ref_len','alt_len')
+                           'ratio1','ratio2','ratio3','number','stat1','stat2','stat3','donor','bc')
+listcl <- listcl %>% mutate(REF = as.character(REF), ALT = as.character(ALT),
+  CONTQ = parse_number(CONTQ), DP = parse_number(DP), ECNT = parse_number(ECNT), GERMQ = sub(".*=", "", GERMQ),
+  MBQ = sub(".*=", "", MBQ), MFRL = sub(".*=", "", MFRL), MMQ = sub(".*=", "", MMQ), MPOS = parse_number(MPOS), 
+  NALOD = parse_number(NALOD), NLOD = parse_number(NLOD), POPAF = parse_number(POPAF), SAAF = sub(".*=", "", SAAF), 
+  SAP = sub(".*=", "", SAP), TLOD = parse_number(TLOD), ANN = sub(".*=", "", ANN))
 
-listcl <- listcl %>% mutate(CONTQ = parse_number(CONTQ), DP = parse_number(DP), ECNT = parse_number(ECNT), GERMQ = sub(".*=", "", GERMQ),
-                           MBQ = sub(".*=", "", MBQ), MFRL = sub(".*=", "", MFRL), MMQ = sub(".*=", "", MMQ), MPOS = parse_number(MPOS), 
-                           NALOD = parse_number(NALOD), NLOD = parse_number(NLOD), POPAF = parse_number(POPAF), SAAF = sub(".*=", "", SAAF), 
-                           SAP = sub(".*=", "", SAP), TLOD = parse_number(TLOD), ANN = sub(".*=", "", ANN))
+pre_filter_num_mut <- nrow(listcl)
+print(listcl)
+listcl <- listcl %>% mutate(ref_len = nchar(REF), alt_len = nchar(ALT)) %>% 
+  filter(ref_len==1 & (
+    (alt_len == 1 & FILTER=='PASS') | (str_detect(ALT, "[ATCG],[ATCG]") & FILTER =='multiallelic')
+    )
+  )
+post_filter_num_mut <- nrow(listcl)
+print(listcl)
 
-
-
-write.csv(listcl, paste(aggregate_out, '/mutations_',sam,'.csv',sep=''))
+filter_nums <- data.frame(pre_filter_num_mut = pre_filter_num_mut,
+  post_filter_num_mut = post_filter_num_mut)
+write.csv(listcl, paste(aggregate_out, '/mutations_',sam,'.csv',sep=''),
+  row.names=F)
+write.csv(filter_nums, paste(aggregate_out, '/filter_nums_',sam,'.csv',sep=''),
+  row.names=F)
  
