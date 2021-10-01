a197cc3244a9f4c91b234ec7f300d38771541315
Humza Hemani <humza.hemani@nih.gov>
2021-10-01 15:52:56 -0400

    step 10 now reports UMI count correctly

--- step10/step10.bash
+++ step10/step10.bash
@@ -3,6 +3,7 @@
 sample_id=${sample_id:def_sample_id}
 exome_sc=${exome_sc:def_exome_sc}
 output_dir=${output_dir:def_output_dir}
+possorted_genome=${possorted_genome:def_possorted_genome}
 
 while [ $# -gt 0 ]; do            
     if [[ $1 == *"--"* ]]; then
@@ -20,5 +21,6 @@ READ_EXOME=$(samtools view -c -F 4 -L `dirname "$0"`/targets_chr.bed ${exome_sc}
 READ_SAM=$(samtools view -c -F 4 ${exome_sc}/${TAG}_SM_bwa.bam)
 COV_EXOME=$(samtools depth -b `dirname "$0"`/targets_chr.bed ${exome_sc}/${TAG}_SM_bwa.bam | wc -l)
 COV=$(samtools depth ${exome_sc}/${TAG}_SM_bwa.bam | wc -l)
+UMI=$(samtools view  ${possorted_genome} | grep -o 'UB:............' | grep -o '..........$' | uniq -c | wc -l)
 echo $TAG, $READ_EXOME, $READ_SAM, $COV_EXOME, $COV, $UMI >> ${output_dir}/summary.csv
 
