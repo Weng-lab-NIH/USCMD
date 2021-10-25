0ddcf174a828b2a7379f56e9133338be34718cbe
HumzaHemani <humzah@umich.edu>
2021-10-25 15:12:59 -0400

    Add files via upload

---
+++ step8/build_step8.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --partition quick
+#SBATCH --time 03:00:00
+#SBATCH --job-name build8
+
+unset SINGULARITY_BINDPATH
+
+module load singularity/3.8.0 
+rm -f step8.sif
+singularity build --remote step8.sif definition_step8.def
