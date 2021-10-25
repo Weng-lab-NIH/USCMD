8675c095057639978e82db538e678caba23d9345
HumzaHemani <humzah@umich.edu>
2021-10-25 15:13:17 -0400

    Add files via upload

---
+++ step9/build_step9.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --partition quick
+#SBATCH --time 03:00:00
+#SBATCH --job-name build9
+#SBATCH --mem 1g
+
+unset SINGULARITY_BINDPATH
+
+module load singularity/3.8.0 
+rm -f step9.sif
+singularity build --remote step9.sif definition_step9_ScoreMutations.def
